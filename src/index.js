686055abae671ae405992cda56aa7fe4349ed391
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-09-11 08:51:19 +0330

    Some bugs were fixed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import { withdraw } from "./features/accounts/accountSlice";
 
-store.dispatch({type: 'account/deposite',payload:250})
-store.dispatch(withdraw(50));
-console.log(store.getState())
+store.dispatch({ type: "account/withdraw", payload: 250 });
+store.dispatch({ type: "account/deposit", payload: 700000000000 });
+var statusBalance = store.getState();
+console.log(statusBalance);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
