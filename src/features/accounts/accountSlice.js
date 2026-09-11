b21dbf1456c5a56566195ae3b709f7600b0dc278
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-09-11 11:13:48 +0330

    the create customer part and the withdraw/deposit part is set

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -11,7 +11,6 @@ export default function accountReducer(state = initialStateAccount, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      //LATER
       return {
         ...state,
         loan: action.payload.amount,
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
 
-store.dispatch({ type: "account/withdraw", payload: 250 });
-store.dispatch({ type: "account/deposit", payload: 700000000000 });
-var statusBalance = store.getState();
-console.log(statusBalance);
+// store.dispatch({ type: "account/withdraw", payload: 250 });
+// store.dispatch({ type: "account/deposit", payload: 700000000000 });
+// var statusBalance = store.getState();
+// console.log(statusBalance);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
