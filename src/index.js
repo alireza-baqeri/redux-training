353d5d871e87c677b475fa39528ffaf679b96da7
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-09-11 08:37:17 +0330

    spliting the redux store and reducer and action functions into multiple files/sub folders

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from "./store";
+import { withdraw } from "./features/accounts/accountSlice";
 
+store.dispatch({type: 'account/deposite',payload:250})
+store.dispatch(withdraw(50));
+console.log(store.getState())
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
